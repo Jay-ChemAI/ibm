0d4af9b2c0f89bfb848fc8aab0828f4b1bb86059
Javier De Santiago <126523418+Jay-ChemAI@users.noreply.github.com>
2023-08-02 15:46:08 -0600

    Add files via upload
    NA

---
+++ APIS.py
@@ -0,0 +1,26 @@
+'''
+Service Oriented Approach
+Most non-trivial web applications use services
+They use services from other applications
+ - Credit card charge 
+ - Hotel Reservation Systems
+Services publish the 'rules' applications must follow to make use of the service (API)
+'''
+
+#WEB SERVICES API
+'''
+APPLICATION PROGRAM INTERFACE
+The API itself is largely abstract in that it specifies an interface and controls the behavior of the objects specified in that interface. The software that provides the functionality described 
+by an API is said to be an 'implementation' of the API. An API is typically defined in terms of the programming language used to build an application
+
+The compute resources to run these APIs are not 'free' 
+The adata provided by these APIs is usually valuable
+The data providers might limit the number of requests per day, demand an API 'key', or even charge for usage
+They might change the rules as things progress..
+ 
+
+
+
+
+
+'''
